e52eda0f7890cb2046b663ae5f6afc56f7ac09d9
eSe <snaker@locolandia.net>
2014-10-13 20:20:00 +0200

    Script for download and save xml files to inspect

---
+++ get_xmls.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#Download raw xml data to inspect
+
+# Stardard tools
+import sys
+import os
+import re
+import logging
+
+from tva import TvaStream, TvaParser
+
+logger = logging.getLogger('movistarxmltv')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('/tmp/movistar.log')
+fh.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+if len(sys.argv) > 1:
+    IP = sys.argv[1]
+    print "Download xml files into /tmp/: "+IP
+else:
+    print "Usage: "+ sys.argv[0]+' MULTICAST_GROUP'
+    exit()
+
+logger.info("Getting xmls")
+
+stream = TvaStream(IP,MCAST_PORT)
+stream.getfiles()
+for i in stream.files().keys():
+    fM3u = open("/tmp/raw_"+IP+"_"+i+".xml", 'w+')
+    fM3u.write(stream.files()[i])
+    fM3u.close
+
+exit()
