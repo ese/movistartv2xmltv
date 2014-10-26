d3e61fab5873a62c50a64c4aeebbb96cf0dc6d08
Sergio Ballesteros <snaker@locolandia.net>
2014-10-26 16:15:07 +0100

    Code clean

--- tv_grab_es_movistar.py
+++ tv_grab_es_movistar.py
@@ -91,9 +91,6 @@ else:
 
     reload(sys)
 
-    SOCK_TIMEOUT = 3
-    FILE_LOG = '/tmp/tv_grab_es_movistar.log'
-
     clientprofile = json.loads(urllib.urlopen("http://172.26.22.23:2001/appserver/mvtv.do?action=getClientProfile").read())['resultData']
     platformprofile = json.loads(urllib.urlopen("http://172.26.22.23:2001/appserver/mvtv.do?action=getPlatformProfile").read())['resultData']
     DEMARCATION =  clientprofile["demarcation"]
@@ -103,67 +100,24 @@ else:
     logger.info("Init. DEM="+str(DEMARCATION)+" TVPACKS="+str(TVPACKAGES)+" ENTRY_MCAST="+MCAST_GRP_START+":"+str(MCAST_PORT))
 
     ENCODING_EPG = 'utf-8'
-    DECODING_EPG = 'latin1'
     ENCODING_SYS = sys.getdefaultencoding()
     sys.setdefaultencoding(ENCODING_EPG)
-
-    # Example, for debugging purpose only
-    programmes = [{'audio': {'stereo': u'stereo'},
-                   'category': [(u'Biz', u''), (u'Fin', u'')],
-                   'channel': u'C23robtv.zap2it.com',
-                   'date': u'2003',
-                   'start': u'20030702000000 ADT',
-                   'stop': u'20030702003000 ADT',
-                   'title': [(u'This Week in Business', u'')]},
-                  {'audio': {'stereo': u'stereo'},
-                   'category': [(u'Comedy', u'')],
-                   'channel': u'C36wuhf.zap2it.com',
-                   'country': [(u'USA', u'')],
-                   'credits': {'producer': [u'Larry David'], 'actor': [u'Jerry Seinfeld']},
-                   'date': u'1995',
-                   'desc': [(u'In an effort to grow up, George proposes marriage to former girlfriend Susan.',
-                             u'')],
-                   'episode-num': (u'7 . 1 . 1/1', u'xmltv_ns'),
-                   'language': (u'English', u''),
-                   'last-chance': (u'Hah!', u''),
-                   'length': {'units': u'minutes', 'length': '22'},
-                   'new': True,
-                   'orig-language': (u'English', u''),
-                   'premiere': (u'Not really. Just testing', u'en'),
-                   'previously-shown': {'channel': u'C12whdh.zap2it.com',
-                                        'start': u'19950921103000 ADT'},
-                   'rating': [{'icon': [{'height': u'64',
-                                         'src': u'http://some.ratings/PGicon.png',
-                                         'width': u'64'}],
-                               'system': u'VCHIP',
-                               'value': u'PG'}],
-                   'star-rating': {'icon': [{'height': u'32',
-                                             'src': u'http://some.star/icon.png',
-                                             'width': u'32'}],
-                                   'value': u'4/5'},
-                   'start': u'20030702000000 ADT',
-                   'stop': u'20030702003000 ADT',
-                   'sub-title': [(u'The Engagement', u'')],
-                   'subtitles': [{'type': u'teletext', 'language': (u'English', u'')}],
-                   'title': [(u'Seinfeld', u'')],
-                   'url': [(u'http://www.nbc.com/')],
-                   'video': {'colour': True, 'aspect': u'4:3', 'present': True,
-                             'quality': 'standard'}}]
-
 
     # Main starts
 
     demarcationstream = TvaStream(MCAST_GRP_START,MCAST_PORT)
     demarcationstream.getfiles()
     demarcationxml = demarcationstream.files()["1_0"]
+
     logger.info("Getting channels source for DEM: "+str(DEMARCATION))
     MCAST_CHANNELS = TvaParser(demarcationxml).get_mcast_demarcationip(DEMARCATION)
 
-    logger.info("Getting channels list from: "+MCAST_CHANNELS)
-    now = datetime.datetime.now()
-    OBJ_XMLTV = ET.Element("tv" , {"date":now.strftime("%Y%m%d%H%M%S"),"source_info_url":"https://go.tv.movistar.es","source_info_name":"Grabber for internal multicast of MovistarTV","generator_info_name":"python-xml-parser","generator_info_url":"http://wiki.xmltv.org/index.php/XMLTVFormat"})
+
+    now = datetime.datetime.utcnow()
+    OBJ_XMLTV = ET.Element("tv" , {"date":now.strftime("%Y%m%d%H%M%S +0000"),"source_info_url":"https://go.tv.movistar.es","source_info_name":"Grabber for internal multicast of MovistarTV","generator_info_name":"python-xml-parser","generator_info_url":"http://wiki.xmltv.org/index.php/XMLTVFormat"})
     #OBJ_XMLTV = ET.Element("tv" , {"date":now.strftime("%Y%m%d%H%M%S")+" +0200"})
 
+    logger.info("Getting channels list from: "+MCAST_CHANNELS)
     channelsstream = TvaStream(MCAST_CHANNELS,MCAST_PORT)
     channelsstream.getfiles()
     xmlchannels = channelsstream.files()["2_0"]
